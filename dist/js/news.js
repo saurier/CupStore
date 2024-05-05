3608055019eb7d8b91ea102140b29a74e0a75a08
Kriz <kruzhanivska.olena212@gmail.com>
2024-05-05 18:39:53 +0300

    CupStore1.0 ready

--- dist/js/news.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.card__content',).forEach((item) => {
-    item.addEventListener('click', () => {
-      item.parentNode.classList.toggle('card__content--active');
-    })
-  });
-
-  function toggle_textchange(e) {
-  toggleText = b=> b.innerHTML = b.innerHTML == "Читати далі" ? "Згорнути" : "Читати далі";
-}
